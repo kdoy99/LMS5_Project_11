4fa759fdd1b2e8bb45fc069c04fa6fec007f10d8
kdy <kdoy990805@gmail.com>
2025-04-09 19:25:02 +0900

    방 생성 창 UI 구성

---
+++ Project_11/Project_11/Project_11/Project_11/Model/RoomInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_11.Model
+{
+    public class RoomInfo
+    {
+        public string Title { get; set; } // 방 제목
+        public DateTime CreatedTime { get; set; } // 만들어진 시간
+        public string Host { get; set; } // 방장 닉네임
+        public int HostRating { get; set; } // 방장 레이팅
+        public string DisplayTime => CreatedTime.ToString("tt h:mm");
+        public string HostInfo => $"{Host} [{HostRating}]";
+    }
+}
